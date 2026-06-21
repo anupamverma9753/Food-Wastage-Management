c179303de8d8b41578142559c2bffe0430105ab6
Anupam Verma <anupamverma9753@gmail.com>
2026-06-21 13:23:20 +0530

    Update queries.py

--- Food Wastage/Source/queries.py
+++ Food Wastage/Source/queries.py
@@ -30,7 +30,7 @@ class SQLQueries:
         f."Food_Type",
         COUNT(CASE WHEN f."Expiry_Date" < CURRENT_DATE THEN 1 END) AS "Expired_Count",
         COUNT(CASE WHEN c."Status" = 'Completed' THEN 1 END) AS "Successfully_Distributed",
-        ROUND(100.0 * COUNT(CASE WHEN f."Expiry_Date" < CURRENT_DATE THEN 1 END) / COUNT(f."Food_ID"), 2) AS     "Wastage_Rate_Percentage"
+        ROUND(100.0 * (count(f."Food_ID")-COUNT(CASE WHEN c."Status" = 'Completed' THEN 1 END)) / COUNT(f."Food_ID"), 2) AS     "Wastage_Rate_Percentage"
     FROM food_listings f
     LEFT JOIN claims c ON f."Food_ID" = c."Food_ID"
     GROUP BY f."Food_Type";
